19e02acc2becd30f7cb5da8fee0d5c972183498d
Wojciech Wesołowski <wojtek.wes@gmail.com>
2021-01-20 17:53:31 +0100

    Modify the UIPointCounter.cs

--- Assets/Scripts/UIPointCounter.cs
+++ Assets/Scripts/UIPointCounter.cs
@@ -11,13 +11,36 @@ namespace DefaultNamespace
         public TextMeshProUGUI textScore;
         public TextMeshProUGUI textChip;
 
+        private string[] _chipNames;
+
+        private void Start()
+        {
+            _chipNames = new string[]
+            {
+                "Chip",
+                "Chip1",
+                "Chip2"
+            };
+        }
+
         private void Update()
         {
             textLives.text = $"Lives: {DataStore.Lives.ToString()}";
             textHp.text = $"HP: {DataStore.HpPoints.ToString()}";
             textScore.text = $"Score: {DataStore.Score.ToString()}";
-            textChip.text = $"x {DataStore.GetItemQuantityFromInventory("Chip")}";
+            textChip.text = $"x {CountChips().ToString()}";
         }
-        
+
+        private int CountChips()
+        {
+            int chipCount = 0;
+            
+            foreach (var chipName in _chipNames)
+            {
+                chipCount += DataStore.GetItemQuantityFromInventory(chipName);
+            }
+
+            return chipCount;
+        }
     }
 }

20a7933ad7ef3ad75d52262d07dbf88eca59c251
Sławomir Brzozowski <brzozasr@interia.eu>
2021-01-25 20:25:56 +0100

    FirstAid class.

---
+++ Assets/Scripts/FirstAid.cs
@@ -0,0 +1,12 @@
+namespace DefaultNamespace
+{
+    public class FirstAid
+    {
+        internal int UniqueId { get; set; }
+        internal string Name { get; set; }
+        internal int RecoveryPoints { get; set; }
+        internal float PosX { get; set; }
+        internal float PosY { get; set; }
+        internal float PosZ { get; set; }
+    }
+}
